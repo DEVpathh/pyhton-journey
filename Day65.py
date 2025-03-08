4cfc77a4ee85028ce77f10bb524738032ea5fdc1
DEVpathh <mukulpathakop@gmail.com>
2025-03-08 22:56:35 +0530

    Add files via upload

---
+++ Day65.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import os
+
+def circularArrayRotation(a, k, queries):
+    n = len(a)
+    k = k % n 
+    result = []
+    
+    for q in queries:
+        result.append(a[(q - k + n) % n])
+    
+    return result
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    q = int(first_multiple_input[2])
+
+    a = list(map(int, input().rstrip().split()))
+
+    queries = []
+
+    for _ in range(q):
+        queries_item = int(input().strip())
+        queries.append(queries_item)
+
+    result = circularArrayRotation(a, k, queries)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
+
