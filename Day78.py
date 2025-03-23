900e61144a891e0218857d692197b29c1a2a808c
DEVpathh <mukulpathakop@gmail.com>
2025-03-23 12:40:59 +0530

    Add files via upload

---
+++ Day78.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import os
+def queensAttack(n, k, r_q, c_q, obstacles):
+    obstacle_positions = {(r, c) for r, c in obstacles}
+    directions = [
+        (-1, 0), (1, 0), (0, -1), (0, 1),
+        (-1, -1), (-1, 1), (1, -1), (1, 1)
+    ]
+    attackable_squares = 0
+    for dr, dc in directions:
+        r, c = r_q + dr, c_q + dc
+        while 1 <= r <= n and 1 <= c <= n and (r, c) not in obstacle_positions:
+            attackable_squares += 1
+            r += dr
+            c += dc
+    return attackable_squares
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    second_multiple_input = input().rstrip().split()
+
+    r_q = int(second_multiple_input[0])
+
+    c_q = int(second_multiple_input[1])
+
+    obstacles = []
+
+    for _ in range(k):
+        obstacles.append(list(map(int, input().rstrip().split())))
+
+    result = queensAttack(n, k, r_q, c_q, obstacles)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
