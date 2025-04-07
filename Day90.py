a275758ff2cfa1cd34af3f62b91da394dca780a0
DEVpathh <mukulpathakop@gmail.com>
2025-04-07 14:38:10 +0530

    Add files via upload

---
+++ Day90.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+import os
+def serviceLane(n, cases):
+    result = []
+    for start, end in cases:
+        result.append(min(width[start:end+1]))
+    return result
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    t = int(first_multiple_input[1])
+
+    width = list(map(int, input().rstrip().split()))
+
+    cases = []
+
+    for _ in range(t):
+        cases.append(list(map(int, input().rstrip().split())))
+
+    result = serviceLane(n, cases)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
