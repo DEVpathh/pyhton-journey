2cca764e68716da6ef7d28a1531449e5837b521f
DEVpathh <mukulpathakop@gmail.com>
2025-04-23 23:04:42 +0530

    Add files via upload

---
+++ Day103.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+import os
+def larrysArray(A):
+    inversions = 0
+    n = len(A)
+    for i in range(n):
+        for j in range(i + 1, n):
+            if A[i] > A[j]:
+                inversions += 1
+    return "YES" if inversions % 2 == 0 else "NO"
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        A = list(map(int, input().rstrip().split()))
+
+        result = larrysArray(A)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
