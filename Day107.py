48b3cb5ff4e057a1d738795884dfeba4cb6338fd
DEVpathh <mukulpathakop@gmail.com>
2025-04-28 20:08:41 +0530

    Add files via upload

---
+++ Day107.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+import os
+def introTutorial(V, arr):
+    for i in range(len(arr)):
+        if arr[i] == V:
+            return i
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    V = int(input().strip())
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = introTutorial(V, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
