6b53e60fc322d04b0e3938f4f1d5f9e78b18c32f
DEVpathh <mukulpathakop@gmail.com>
2025-06-13 20:29:17 +0530

    Add files via upload

---
+++ Day145.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+import os
+def commonChild(s1, s2):
+    n = len(s1)
+    dp = [[0] * (n + 1) for _ in range(n + 1)]
+
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            if s1[i - 1] == s2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+    
+    return dp[n][n]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s1 = input()
+
+    s2 = input()
+
+    result = commonChild(s1, s2)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
