6f08194d5266db6384c75c7023a70276512d78af
DEVpathh <mukulpathakop@gmail.com>
2025-06-27 10:16:17 +0530

    Add files via upload

---
+++ Day148.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+import os
+from collections import Counter
+
+def missingNumbers(arr, brr):
+    count_arr = Counter(arr)
+    count_brr = Counter(brr)
+    
+    missing = []
+    for num in count_brr:
+        if count_brr[num] > count_arr.get(num, 0):
+            missing.append(num)
+    
+    return sorted(missing)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    m = int(input().strip())
+
+    brr = list(map(int, input().rstrip().split()))
+
+    result = missingNumbers(arr, brr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
